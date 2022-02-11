1bf3b1560237b26c8dbe2f0d4468aaab2b17dd66
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-02-11 15:54:55 +0100

    [ADD] header static version

--- front/src/components/Layout/Header/index.js
+++ front/src/components/Layout/Header/index.js
@@ -2,10 +2,62 @@ import React from "react";
 
 import "./header.scss";
 
+// pictures
+import Logo from "../../../assets/img/logo.png";
+import Volunteer from "../../../assets/img/devenir_benevole.png";
+import Donate from "../../../assets/img/faire_un_don.png";
+
+// Feather
+import { Facebook, Instagram, Linkedin, Mail, Twitter, Youtube } from "react-feather";
+
 const Header = () => {
     return (
-        <header className="header">
-            <h1>Header</h1>
+        <header id="header">
+            <div id="header-social">
+                <Facebook className="header-social__item" color="white" />
+                <Twitter className="header-social__item" color="white" />
+                <Youtube className="header-social__item" color="white" />
+                <Instagram className="header-social__item" color="white" />
+                <Linkedin className="header-social__item" color="white" />
+                <Mail className="header-social__item" color="white" />
+            </div>
+            <section id="header-main">
+                <div id="header-main-main">
+                    <div id="header-main__logo">
+                        <img src={Logo} alt="logo association Valentin Haüy" />
+                    </div>
+                    <div id="header-main__title">
+                        <p id="title-assoc">Association</p>
+                        <h1 id="title-vh">Valentin Haüy</h1>
+                        <p id="title-cc">Caen Calvados</p>
+                        <p id="title-orga">Organisation à but non lucratif</p>
+                    </div>
+                    <div id="header-main__join">
+                        <img src={Volunteer} alt="devenir bénévole" className="join-picture" />
+                        <img src={Donate} alt="faire un don" className="join-picture" />
+                    </div>
+                </div>
+                <div id="header-main__bg" />
+            </section>
+            <nav id="header-nav">
+                <ul>
+                    <li>
+                        <a href="#">Association Valentin Haüy</a>
+                    </li>
+                    <li>
+                        <a href="#">Actualités</a>
+                    </li>
+                    <li>
+                        <a href="#">Activités</a>
+                    </li>
+                    <li>
+                        <a href="#">Mécénat partenariat</a>
+                    </li>
+                    <li>
+                        <a href="#">La boutique</a>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
