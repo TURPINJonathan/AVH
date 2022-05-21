a148d94fda9217274ef2eb24db07bf80a337b4b5
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-05-21 17:50:48 +0200

    [UPDATE] Dashboard informations && [ADD] htaccess to manage reload page

--- back/src/Controller/Admin/DashboardController.php
+++ back/src/Controller/Admin/DashboardController.php
@@ -51,17 +51,17 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Actualité', 'fa fa-newspaper', Actualite::class);
+        yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-duotone fa-user', User::class);
+        yield MenuItem::linkToCrud('Actualités', 'fa fa-newspaper', Actualite::class);
+        yield MenuItem::linkToCrud('Boutique', 'fa fa-duotone fa-shop', Boutique::class);
+        yield MenuItem::linkToCrud('Missions', 'fa fa-duotone fa-user-secret', Mission::class);
+        yield MenuItem::linkToCrud('Partenaires', 'fa fa-duotone fa-handshake', Partenaire::class);
         yield MenuItem::linkToCrud('AVH Caen', 'fa fa-duotone fa-location-crosshairs', AvhCaen::class);
         yield MenuItem::linkToCrud('AVH Compte Rendu', 'fa fa-duotone fa-file-pen', AvhCompteRendu::class);
         yield MenuItem::linkToCrud('AVH National', 'fa fa-duotone fa-earth-europe', AvhNational::class);
-        yield MenuItem::linkToCrud('Boutique', 'fa fa-duotone fa-shop', Boutique::class);
-        yield MenuItem::linkToCrud('Faq', 'fa fa-duotone fa-question', Faq::class);
-        yield MenuItem::linkToCrud('Legal', 'fa fa-duotone fa-scale-balanced', Legal::class);
-        yield MenuItem::linkToCrud('Mission', 'fa fa-duotone fa-user-secret', Mission::class);
-        yield MenuItem::linkToCrud('Partenaire', 'fa fa-duotone fa-handshake', Partenaire::class);
-        yield MenuItem::linkToCrud('Utilisateur', 'fa fa-duotone fa-user', User::class);
+        yield MenuItem::linkToCrud('FAQ', 'fa fa-duotone fa-question', Faq::class);
+        yield MenuItem::linkToCrud('Législation', 'fa fa-duotone fa-scale-balanced', Legal::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
