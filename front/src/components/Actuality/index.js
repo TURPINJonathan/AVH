fc10719b10ca80131ab0f6497cde49eb4876f66e
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-05-24 22:29:40 +0200

    update htacess

--- front/src/components/Actuality/index.js
+++ front/src/components/Actuality/index.js
@@ -3,12 +3,39 @@ import React from "react";
 import './actuality.scss';
 
 import loading from '../../assets/img/page_developping.png';
+import defi_voile from "../../assets/img/defi_voile.jpg";
 
 const Actuality = () => {
     return (
         <main className="actuality">
             <h2>Actualités</h2>
-            <img className="loading" src={loading} alt="Page en cours de construction" />
+            <div className="actuality_content">
+                <aside className="search">
+                    <img src={defi_voile} alt="loading" />
+                </aside>
+                <article className="allArticle">
+                    <div className="anArticle">
+                        <img className="anArticle_picture" src={loading} alt="loading" />
+                        <h4>Titre de l'article</h4>
+                        <button className="anArticle_button" />
+                    </div>
+                    <div className="anArticle">
+                        <img className="anArticle_picture" src={loading} alt="loading" />
+                        <h4>Titre de l'article</h4>
+                        <button className="anArticle_button" />
+                    </div>
+                    <div className="anArticle">
+                        <img className="anArticle_picture" src={loading} alt="loading" />
+                        <h4>Titre de l'article</h4>
+                        <button className="anArticle_button" />
+                    </div>
+                    <div className="anArticle">
+                        <img className="anArticle_picture" src={loading} alt="loading" />
+                        <h4>Titre de l'article</h4>
+                        <button className="anArticle_button" />
+                    </div>
+                </article>
+            </div>
         </main>
     );
 }
