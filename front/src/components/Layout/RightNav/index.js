f888f6de60ec71ac98e344cd892ff2f8535f493b
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-05-28 14:48:37 +0200

    [UPDATE] RightNav links

--- front/src/components/Layout/RightNav/index.js
+++ front/src/components/Layout/RightNav/index.js
@@ -11,30 +11,30 @@ import "./rightNav.scss";
 const RightNav = () => {
     return (
         <aside id="rightNav">
-            <NavLink to={`/`}>
+            <a href="https://www.facebook.com/avhcaen" target="_blank" rel="noreferrer">
                 <ReactTooltip
                     place="left"
                     type="dark"
                     effect="float"
                 />
                 <Facebook className="rightNav-item" data-tip="Facebook" />
-            </NavLink>
-            <NavLink to={`/`}>
+            </a>
+            <a href="https://www.instagram.com/avhcaen" target="_blank" rel="noreferrer">
                 <ReactTooltip
                     place="left"
                     type="dark"
                     effect="float"
                 />
                 <Instagram className="rightNav-item" data-tip="Instagram" />
-            </NavLink>
-            <NavLink to={`/`}>
+            </a>
+            <a href="https://twitter.com/avhcaen" target="_blank" rel="noreferrer">
                 <ReactTooltip
                     place="left"
                     type="dark"
                     effect="float"
                 />
                 <Twitter className="rightNav-item" data-tip="Twitter" />
-            </NavLink>
+            </a>
             <NavLink to={`/`}>
                 <ReactTooltip
                     place="left"
@@ -43,14 +43,14 @@ const RightNav = () => {
                 />
                 <Youtube className="rightNav-item" data-tip="Youtube" />
             </NavLink>
-            <NavLink to={`/`} className="heart">
+            <a href="https://sensibiliser.avh.asso.fr/campagne-2018/" target="_blank" rel="noreferrer" className="heart">
                 <ReactTooltip
                     place="left"
                     type="dark"
                     effect="float"
                 />
                 <Heart className="heart-hover" data-tip="Faire un don" />
-            </NavLink>
+            </a>
         </aside>
     );
 };
