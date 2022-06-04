fe227f73688f2a8cec64d8d688f4f1022f4d4f64
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-06-04 12:36:27 +0200

    [UPDATE] mission presentation

--- back/src/Controller/Admin/MissionCrudController.php
+++ back/src/Controller/Admin/MissionCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Mission;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -20,18 +21,25 @@ class MissionCrudController extends AbstractCrudController
         return Mission::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Mission')
+            ->setEntityLabelInPlural('Missions')
+            ->setSearchFields(['nom', 'overview', 'detail', 'show_main', 'slug']);
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id')->hideOnForm()->hideOnIndex(),
             TextField::new('nom'),
-            TextEditorField::new('overview'),
-            TextareaField::new('detail')->hideOnIndex(),
-            BooleanField::new('show_main'),
-            TextField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex(),
+            TextField::new('overview')->setLabel('Courte description')->setHelp('100 caractères maximum')->setMaxLength(100),
+            TextEditorField::new('detail')->setLabel('Détails')->hideOnIndex(),
+            TextField::new('imageFile')->setLabel('Image de la mission')->setFormType(VichImageType::class)->hideOnIndex()->onlyWhenCreating()->setHelp('Pour une meilleure visibilité, l\'image doit être au format jpg, jpeg, png ou gif et doit faire moins de 2Mo.'),
+            BooleanField::new('show_main')->setLabel('Afficher également en page d\'accueil ?')->setHelp('Si oui, elle sera automatiquement retravailler en noir et blanc. Souhaitez-vous l\'afficher dans la page d\'accueil ?'),
             ImageField::new('file')->setBasePath('/uploads/mission_image')->onlyOnIndex(),
-            SlugField::new('slug')->setTargetFieldName('nom'),
+            SlugField::new('slug')->setTargetFieldName('nom')->setHelp('Apparaîtra dans l\'URL de la page de la mission')->hideOnIndex(),
         ];
     }
 }
