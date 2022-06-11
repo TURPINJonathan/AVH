87b6d803a81c387b82787d510fbdebbb1ca0f14b
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-06-11 17:57:32 +0200

    [UPDATE] url

--- front/src/middlewares/mainMiddleware.js
+++ front/src/middlewares/mainMiddleware.js
@@ -6,7 +6,7 @@ import { DEV_URL, url } from '../data';
 const mainMiddleware = (store) => (next) => (action) => {
     switch (action.type) {
         case FETCH_MISSION:
-            axios.get(`${DEV_URL}mission/list`)
+            axios.get(`${url}mission/list`)
                 .then((response) => {
                     // console.log('Middleware : ' + response.data);
                     store.dispatch(saveMission(response.data));
