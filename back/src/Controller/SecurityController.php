207da9ae3fcec7cdac406a75b030b5e60c7c08c3
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-06-12 01:39:11 +0200

    [UPDATE] login road

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: 'back/login', name: 'app_login')]
+    #[Route(path: '/back/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
