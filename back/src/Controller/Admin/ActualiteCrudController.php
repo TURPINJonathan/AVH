3e64080ac3aa98712dca1ec0ffcd7ae45b387550
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-06-16 20:46:59 +0200

    user on actuality

--- back/src/Controller/Admin/ActualiteCrudController.php
+++ back/src/Controller/Admin/ActualiteCrudController.php
@@ -147,6 +147,8 @@ class ActualiteCrudController extends AbstractCrudController
             // AssociationField::new('tags')
             // ->setLabel('Tags')
             // ->setHelp('Choisissez les tags qui apparaîtront sur l\'actualité.'),
+
+            FormField::addPanel('Qui êtes-vous ?'),
             AssociationField::new('users')
                 ->setLabel('Auteurs')
                 ->setHelp('Choisissez l\'auteur de l\'actualité.')
